18546d3627792ef6bd47ae8b6f97bbe808a0ea02
Pavel Yakovlev <p_yak_11@mail.ru>
2021-04-14 03:23:51 +0300

    Update daal4py to 2021.1 (#17)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,6 @@ set DAAL4PY_VERSION=%PKG_VERSION%
 set MPIROOT=%PREFIX%\Library
 IF DEFINED DAALROOT (set DALROOT=%DAALROOT%)
 IF NOT DEFINED DALROOT (set DALROOT=%PREFIX%)
-set PATH=%PATH%;%PREFIX%\Library\bin\libfabric
+set PATH=%PREFIX%\Library\bin;%PATH%;%PREFIX%\Library\bin\libfabric
 
 %PYTHON% setup.py install --single-version-externally-managed --record record.txt
