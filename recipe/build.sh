9671778842f3cb2df4fbaec1c8abb94c6164619d
Kirill <kirill.petrov@intel.com>
2021-07-04 14:58:57 +0300

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -23,8 +23,8 @@ if [ -z "${DALROOT}" ]; then
     export DALROOT=${PREFIX}
 fi
 
-export CC=x86_64-conda-linux-gnu-cc
-export CXX=x86_64-conda-linux-gnu-c++
+export CC=gcc
+export CXX=g++
 
 export DAAL4PY_VERSION=$PKG_VERSION
 export MPIROOT=${PREFIX}
